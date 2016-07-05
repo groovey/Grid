abdf952142d619cf81a503a5740a6b3e66af2ab9
pokoot <pokoot@gmail.com>
2016-07-06 03:58:18 +0800

    Beta changes

--- index.php
+++ index.php
@@ -4,12 +4,39 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use Groovey\Grid\Grid;
 
-$grid = new Grid();
+$grid = new Grid(
+        $templatesPath = __DIR__. '/templates/grid',
+        $cachePath     = __DIR__. '/cache'
+    );
 
 
 $grid->load('./config/sample.yml');
 
+$datas = [
+                ['id' => 1, 'name' => 'Sample 1'],
+                ['id' => 2, 'name' => 'Sample 2'],
+                ['id' => 3, 'name' => 'Sample 3'],
+            ];
 
-print $grind->render('listing');
+// $listingHtml = $grid->render
 
+// print $grid->render('listing');
+?>
+<table class="" border="1" cellspacing="1" cellspacing="1">
+    <thead>
+        <?= $grid->renderListingHeader(); ?>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="">Gecko</td>
+            <!--  <td class="">Firefox 1.0</td>
+            <td class="">Win 98+ / OSX.2+</td>
+            <td class="center ">1.7</td>
+            <td class="text-center">
+                <button class="btn btn-default btn-rounded btn-outline btn-xs"><i class="fa fa-times"></i></button>
+                <button class="btn btn-default btn-rounded btn-outline btn-xs"><i class="fa fa-check"></i></button>
+            </td> -->
+        </tr>
+    </tbody>
+</table>
 
--- src/Grid.php
+++ src/Grid.php
@@ -6,16 +6,64 @@ use Symfony\Component\Yaml\Yaml;
 
 class Grid {
 
-    public function __construct()
-    {
+    private $twig;
+    private $yaml;
+    private $html;
 
+    public function __construct($templatesPath, $cachePath)
+    {
+        $cache  = ($cachePath) ? ['cache' => $cachePath] : [];
+        $loader = new \Twig_Loader_Filesystem($templatesPath);
+        $twig   = new \Twig_Environment($loader, $cache);
+
+        $this->twig = $twig;
     }
 
     public function load($config)
     {
-        $yaml = Yaml::parse(file_get_contents($config));
+        $this->yaml = Yaml::parse(file_get_contents($config));
+    }
 
-        print_r( $yaml);
+    public function renderListingHeader()
+    {
+        $twig = $this->twig;
+        $yaml = $this->yaml;
+
+        $html = '';
+        foreach ($yaml['listing'] as $value) {
+            $header = $value['header'];
+
+            extract($header);
+
+            $html .= $twig->render('listing/header.html', [
+                        'label'   => coalesce($label),
+                        'width'   => coalesce($label),
+                    ]);
+        }
+        return $html;
+    }
+
+    public function renderListingData($datas)
+    {
+        $twig = $this->twig;
+        $yaml = $this->yaml;
+
+        $html = '';
+        // foreach ($yaml['data'] as $value) {
+        //     $data = $value['data'];
+
+        //     extract($data);
+
+        //     $html .= $twig->render('listing/data.html', [
+        //                     'value' => coalesce($label),
+        //                 ]);
+        // }
+        return $html;
+
+    }
+
+    public function render()
+    {
 
     }
 
